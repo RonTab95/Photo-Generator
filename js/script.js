d9e6c6902bb95ede813f0865cd6db18368751d02
Rontab <rontab95@yahoo.com>
2022-01-20 21:50:45 -0800

    Added the pictures

--- js/script.js
+++ js/script.js
@@ -4,14 +4,47 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 
-const getImage = async function () {
-    const res = await fetch(
-        "https://picsum.photos/v2/list?limit=100"
-    );
+  const selectRandomImage = function(images){
+
+    let randomIndex =Math.floor(Math.random()*images.length);
+    let randomImage = images[randomIndex];
+    displayImage(randomImage);
+
+};
+
+const displayImage = function(randomImage){
+     
+    let author = randomImage.author;
+    let imageAddress = randomImage.download_url;
+    
+    imgDiv.classList.remove("hide");
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    
+
+
+
+
+};
+
+button.addEventListener("click", function(){
+
+    
+    const getImage = async function () {
+        const res = await fetch(
+            "https://picsum.photos/v2/list?limit=100"
+        );
+      
+        const images = await res.json();
+        selectRandomImage(images);
+      };
+      
+
+      getImage();
+
+
+});
+
+ 
   
-    const images = await res.json();
-    console.log(images);
-  };
-  
-  getImage();
   
